06b12f837de9a94f24260b4b81ae14185f6485dd
trizin <25263018+trizin@users.noreply.github.com>
2022-05-30 10:42:07 +0000

    Update structure

--- db/structure.js
+++ db/structure.js
@@ -1,7 +1,7 @@
 var poolInfoTable = `CREATE TABLE pool_info(
     chainID        INT  NOT NULL
     ,basetoken      VARCHAR(94) NOT NULL
-    ,pool_addr      VARCHAR(94) NOT NULL PRIMARY KEY
+    ,pool_addr      VARCHAR(94) NOT NULL
    ,vol_amt        FLOAT(25,10) NOT NULL
    ,stake_amt      FLOAT(25,10) NOT NULL
    ,nft_addr       VARCHAR(94) NOT NULL
@@ -9,22 +9,23 @@ var poolInfoTable = `CREATE TABLE pool_info(
    ,DT_symbol      VARCHAR(94) NOT NULL
    ,basetoken_addr VARCHAR(94) NOT NULL
    ,did            VARCHAR(71) NOT NULL
-   ,url            VARCHAR(113) NOT NULL);`;
+   ,url            VARCHAR(113) NOT NULL,
+   PRIMARY KEY(chainID, pool_addr) );`;
 
 var poolVolsTable = `CREATE TABLE pool_vols(
-    recordId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     chainID        INT  NOT NULL
    ,basetoken      VARCHAR(94) NOT NULL
    ,pool_addr      VARCHAR(94) NOT NULL
-   ,vol_amt        FLOAT(25,10) NOT NULL);`;
+   ,vol_amt        FLOAT(25,10) NOT NULL,
+   PRIMARY KEY(chainID, pool_addr) );`;
 
 var stakesChain = `CREATE TABLE pool_stakes(
-    recordId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     chainID        INT  NOT NULL
    ,basetoken      VARCHAR(94) NOT NULL
    ,LP_addr        VARCHAR(94) NOT NULL
    ,pool_addr      VARCHAR(94) NOT NULL
-   ,stake_amt        FLOAT(25,10) NOT NULL)`;
+   ,stake_amt        FLOAT(25,10) NOT NULL,
+   PRIMARY KEY(chainID, pool_addr, LP_addr) )`;
 
 module.exports = {
     poolInfoTable,
