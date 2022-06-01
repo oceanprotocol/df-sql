432ea156ff46d4fa8defd73426f85b98265ab587
trizin <25263018+trizin@users.noreply.github.com>
2022-06-01 12:57:25 +0000

    Hotfix typo

--- src/services/pool_stake/index.js
+++ src/services/pool_stake/index.js
@@ -1,15 +1,7 @@
 const { selectQuery } = require("../querier");
 
-const getStakes = ({ jsonsql, sort, limit, offset, group, fields }) => {
-  return selectQuery(
-    jsonsql,
-    sort,
-    limit,
-    offset,
-    "pool_stakes",
-    group,
-    fields
-  );
+const getStakes = ({ query, sort, limit, offset, group, fields }) => {
+  return selectQuery(query, sort, limit, offset, "pool_stakes", group, fields);
 };
 
 module.exports = {
