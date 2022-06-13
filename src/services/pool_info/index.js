5218ff41e62788a8063edfc861c593181c62ca8c
trizin <25263018+trizin@users.noreply.github.com>
2022-06-13 10:10:24 +0000

    Formatting

--- src/services/pool_info/index.js
+++ src/services/pool_info/index.js
@@ -1,7 +1,16 @@
 const { selectQuery } = require("../querier");
 
-const getPools = ({ query, sort, limit, offset, group, fields,join }) => {
-  return selectQuery(query, sort, limit, offset, "pool_info", group, fields,join);
+const getPools = ({ query, sort, limit, offset, group, fields, join }) => {
+  return selectQuery(
+    query,
+    sort,
+    limit,
+    offset,
+    "pool_info",
+    group,
+    fields,
+    join
+  );
 };
 
 module.exports = {
