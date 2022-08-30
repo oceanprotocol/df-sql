0607987e197892835b4a0ac5410368f15f4c6a28
trizin <25263018+trizin@users.noreply.github.com>
2022-08-30 13:21:15 +0000

    Add `nft_info` to app

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const allocationsRouter = require("./allocations");
 const vebalsRouter = require("./vebals");
 const nftvolsRouter = require("./nft_vols");
 const rewardsInfoRouter = require("./rewards_info");
+const nftInfoRouter = require("./nft_info");
 
 app.use(cors());
 app.use(express.json({ limit: "100mb" }));
@@ -14,5 +15,6 @@ app.use(allocationsRouter);
 app.use(vebalsRouter);
 app.use(nftvolsRouter);
 app.use(rewardsInfoRouter);
+app.use(nftInfoRouter);
 
 app.listen(6234);
