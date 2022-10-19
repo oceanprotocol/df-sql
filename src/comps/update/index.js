4c97c21f77ec70bfbaca74104ed14d0183b9850c
trizin <25263018+trizin@users.noreply.github.com>
2022-10-19 19:24:06 +0300

    Remove double quotes

--- src/comps/update/index.js
+++ src/comps/update/index.js
@@ -14,7 +14,7 @@ async function updateDb(data, dbname) {
     let query = `INSERT INTO ${dbname} (${keys.join(", ")}) VALUES (${values
       .map((x) => {
         if (isNaN(x) || x.toString().startsWith("0x")) {
-          let str = `"${x}"`
+          let str = `${x}`
           str = str.replace(/%@#/g, ',');
           str = db.escape(str)
           return str
