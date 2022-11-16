db00c54f4f2763d1fbc77f5c8a932fd4cd796c8f
trizin <25263018+trizin@users.noreply.github.com>
2022-11-16 10:25:05 +0000

    Storage module

---
+++ src/comps/fs/storage.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+let storage_file = "/tmp/storage_dfsql.json"
+
+function saveStorage(key, value) {
+    let storage = {}
+    if (fs.existsSync(storage_file)) {
+        storage = loadStorage()
+    }
+    storage[key] = value
+    fs.writeFileSync(storage_file, JSON.stringify(storage))
+}
+
+function loadStorage() {
+    let data = fs.readFileSync(storage_file, "utf-8")
+    return JSON.parse(data)
+}
+
+function readStorage(key) {
+    let storage = loadStorage()
+    return storage[key]
+}
+
+module.exports = { saveStorage, readStorage }
