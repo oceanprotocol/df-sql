607e98d579ac89d9a33e8101a6dc2319cd260c1b
trizin <25263018+trizin@users.noreply.github.com>
2022-11-28 14:09:12 +0000

    Add batch update round function

---
+++ src/comps/update/batch.js
@@ -0,0 +1,23 @@
+const batchUpdateRound = async ({
+    allocations,
+    nftvols,
+    vebals,
+    rewardsInfo,
+    nftinfo,
+    roundNumber
+}) => {
+    await cleanDb("allocations", roundNumber)
+    await updateDb(allocations, "allocations", roundNumber)
+
+    await cleanDb("nft_vols", roundNumber)
+    await updateDb(nftvols, "nft_vols", roundNumber)
+
+    await cleanDb("vebals", roundNumber)
+    await updateDb(vebals, "vebals", roundNumber)
+
+    await cleanDb("rewards_info", roundNumber)
+    await updateDb(rewardsInfo, "rewards_info", roundNumber)
+
+    await cleanDb("nft_info", roundNumber)
+    await updateDb(nftinfo, "nft_info", roundNumber)
+}
