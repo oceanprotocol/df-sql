649baae16fa2ff344acf56af6ff8f7bc2f91e05b
trizin <25263018+trizin@users.noreply.github.com>
2022-11-28 14:09:34 +0000

    Use batch update round and formatting

--- src/cron/main.js
+++ src/cron/main.js
@@ -1,82 +1,74 @@
-const croner = require("croner");
-const { calcApyPerAsset, calcGeneralApy } = require("../comps/apy/calc");
-const { calculateAllocations, calculateVolumes } = require("../comps/calc/data");
-const { readDataDir } = require("../comps/fs/dir");
-const { saveStorage } = require("../comps/fs/storage");
-const { updateDb, cleanDb } = require("../comps/update");
+const croner = require("croner")
+const { calcApyPerAsset, calcGeneralApy } = require("../comps/apy/calc")
+const { calculateAllocations, calculateVolumes } = require("../comps/calc/data")
+const { readDataDir } = require("../comps/fs/dir")
+const { saveStorage } = require("../comps/fs/storage")
+const { updateDb, cleanDb } = require("../comps/update")
 
 croner.Cron("0 */5 * * * *", () => {
-  sync();
-});
+    sync()
+})
 
-const dataDir = "/csv/";
+const dataDir = "/csv/"
 
 async function sync() {
-  console.log("Starting sync");
+    console.log("Starting sync")
 
-  let {
-    allocations,
-    allocations_realtime,
-    nftvols,
-    vebals,
-    vebals_realtime,
-    rewardsInfo,
-    nftinfo,
-    rates,
-    symbols,
-  } = readDataDir(dataDir);
+    let {
+        allocations,
+        allocations_realtime,
+        nftvols,
+        vebals,
+        vebals_realtime,
+        rewardsInfo,
+        nftinfo,
+        rates,
+        symbols
+    } = readDataDir(dataDir)
 
+    try {
+        nftinfo = calculateAllocations({
+            allocations,
+            vebals_realtime,
+            allocations_realtime,
+            allocations,
+            vebals,
+            nftinfo
+        })
+    } catch (error) {
+        console.error("Error calculating nft allocations", error)
+    }
 
-  try {
-    nftinfo = calculateAllocations({
-      allocations,
-      vebals_realtime,
-      allocations_realtime,
-      allocations,
-      vebals,
-      nftinfo
+    try {
+        nftvols = calculateVolumes({
+            nftvols,
+            rates,
+            symbols
+        })
+    } catch (error) {
+        console.error("Error calculating nft volumes", error)
+    }
+
+    try {
+        nftinfo = calcApyPerAsset({
+            nftinfo,
+            rewardsInfo
+        })
+        let generalApy = calcGeneralApy({
+            nftinfo,
+            rewardsInfo
+        })
+        saveStorage("generalApy", generalApy)
+    } catch (error) {
+        console.error("Error calculating APY", error)
+    }
+
+    await batchUpdateRound({
+        allocations,
+        nftvols,
+        vebals,
+        rewardsInfo,
+        nftinfo,
+        roundNumber: 0 // 0 is the current round
     })
-  } catch (error) {
-    console.error("Error calculating nft allocations", error);
-  }
-
-  try {
-    nftvols = calculateVolumes({
-      nftvols,
-      rates,
-      symbols
-    })
-  } catch (error) {
-    console.error("Error calculating nft volumes", error);
-  }
-
-  try {
-    nftinfo = calcApyPerAsset({
-      nftinfo,
-      rewardsInfo
-    })
-    let generalApy = calcGeneralApy({
-      nftinfo,
-      rewardsInfo
-    })
-    saveStorage("generalApy", generalApy);
-  } catch (error) {
-    console.error("Error calculating APY", error);
-  }
-
-
-  await cleanDb("allocations");
-  await updateDb(allocations, "allocations");
-
-  await cleanDb("nft_vols");
-  await updateDb(nftvols, "nft_vols");
-
-  await cleanDb("vebals");
-  await updateDb(vebals, "vebals");
-
-  await cleanDb("rewards_info");
-  await updateDb(rewardsInfo, "rewards_info");
-
-  await cleanDb("nft_info");
-  await updateDb(nftinfo, "nft_info");
 }
