73104eb4c01e0aa83784d7c6ea80ac337c4d19ae
trizin <25263018+trizin@users.noreply.github.com>
2022-11-28 14:02:16 +0000

    Formatting

--- src/comps/update/index.js
+++ src/comps/update/index.js
@@ -1,35 +1,34 @@
-const { parseCsv } = require("../csv/parse");
-const db = require("../../db");
+const { parseCsv } = require("../csv/parse")
+const db = require("../../db")
 
 async function cleanDb(dbname) {
-  await db.promise().query(`DELETE FROM ${dbname}`);
+    await db.promise().query(`DELETE FROM ${dbname}`)
 }
 
 async function updateDb(data, dbname) {
-  console.log("Updating db", dbname);
-  data.forEach(async (element) => {
-    let keys = Object.keys(element);
-    let values = Object.values(element);
+    console.log("Updating db", dbname)
+    data.forEach(async (element) => {
+        let keys = Object.keys(element)
+        let values = Object.values(element)
 
-    let query = `INSERT INTO ${dbname} (${keys.join(", ")}) VALUES (${values
-      .map((x) => {
-        if (isNaN(x) || x.toString().startsWith("0x")) {
-          let str = `${x}`
-          str = str.replace(/%@#/g, ',');
-          str = db.escape(str)
-          return str
-        } else {
-          return parseFloat(x)
-        }
-      }
-      )
-      .join(", ")})`;
+        let query = `INSERT INTO ${dbname} (${keys.join(", ")}) VALUES (${values
+            .map((x) => {
+                if (isNaN(x) || x.toString().startsWith("0x")) {
+                    let str = `${x}`
+                    str = str.replace(/%@#/g, ",")
+                    str = db.escape(str)
+                    return str
+                } else {
+                    return parseFloat(x)
+                }
+            })
+            .join(", ")})`
 
-    let res = await db.promise().query(query);
-  });
+        let res = await db.promise().query(query)
+    })
 }
 
 module.exports = {
-  updateDb,
-  cleanDb,
-};
+    updateDb,
+    cleanDb
+}
