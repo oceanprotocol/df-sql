d5369eb2dcbd78f908bfb91752fbad316566abfd
trizin <25263018+trizin@users.noreply.github.com>
2022-11-28 10:55:46 +0000

    Read dir module

---
+++ src/comps/fs/dir.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+
+function readDataDir(dataDir) {
+  let allocations = [];
+  let allocations_realtime = [];
+  let nftvols = [];
+  let vebals = [];
+  let vebals_realtime = [];
+  let rewardsInfo = [];
+  let nftinfo = [];
+
+  let rates = [];
+  let symbols = [];
+
+  let files = fs.readdirSync(dataDir);
+  for (let file of files) {
+    if (file.includes("allocations")) {
+      if (file.includes("realtime")) {
+        allocations_realtime.push(...parseCsv(`${dataDir}${file}`));
+      } else {
+        allocations.push(...parseCsv(`${dataDir}${file}`));
+      }
+    }
+    if (file.includes("nftvols")) {
+      nftvols.push(...parseCsv(`${dataDir}${file}`));
+    }
+    if (file.includes("vebals")) {
+      if (file.includes("realtime")) {
+        vebals_realtime.push(...parseCsv(`${dataDir}${file}`));
+      } else {
+        vebals.push(...parseCsv(`${dataDir}${file}`));
+      }
+    }
+    if (file.includes("rewardsinfo")) {
+      rewardsInfo.push(...parseCsv(`${dataDir}${file}`));
+    }
+    if (file.includes("nftinfo")) {
+      nftinfo.push(...parseCsv(`${dataDir}${file}`));
+    }
+    if (file.includes("rate-")) {
+      rates.push(...parseCsv(`${dataDir}${file}`));
+    }
+    if (file.includes("symbols-")) {
+      symbols.push(...parseCsv(`${dataDir}${file}`));
+    }
+  }
+
+  return {
+    allocations,
+    allocations_realtime,
+    nftvols,
+    vebals,
+    vebals_realtime,
+    rewardsInfo,
+    nftinfo,
+    rates,
+    symbols,
+  };
+}
+
+module.exports = { readDataDir };
