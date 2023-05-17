5c703e9ee8f550fe9ba06098e8924134a051e6a4
trizin <25263018+trizin@users.noreply.github.com>
2023-05-17 13:02:09 +0300

    Create tests for CSV parsing functionality

---
+++ test/csv_parse.test.js
@@ -0,0 +1,62 @@
+const { createObject, parseCsv } = require("../src/comps/csv/parse.js")
+const fs = require("fs")
+
+jest.mock("fs")
+
+describe("Testing CSV Parser", () => {
+    test("test createObject function", () => {
+        const keys = ["name", "age", "gender"]
+        const values = ["John", "25", "Male"]
+        const expected = {
+            name: "John",
+            age: "25",
+            gender: "Male"
+        }
+
+        const obj = createObject(keys, values)
+        expect(obj).toEqual(expected)
+    })
+
+    test("test parseCsv function", () => {
+        const mockCsvData = "name,age,gender\nJohn,25,Male\nJane,30,Female"
+        fs.readFileSync.mockReturnValue(mockCsvData)
+
+        const expected = [
+            {
+                name: "John",
+                age: "25",
+                gender: "Male"
+            },
+            {
+                name: "Jane",
+                age: "30",
+                gender: "Female"
+            }
+        ]
+
+        const result = parseCsv("some_path")
+        expect(result).toEqual(expected)
+    })
+
+    test("test parseCsv function with missing data", () => {
+        const mockCsvData =
+            "name,age,gender\nJohn,25,Male\nJane,30,Female\n,35,Male"
+        fs.readFileSync.mockReturnValue(mockCsvData)
+
+        const expected = [
+            {
+                name: "John",
+                age: "25",
+                gender: "Male"
+            },
+            {
+                name: "Jane",
+                age: "30",
+                gender: "Female"
+            }
+        ]
+
+        const result = parseCsv("some_path")
+        expect(result).toEqual(expected)
+    })
+})
