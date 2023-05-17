820f63610c280f86d1cc7c7e6dea17d51b1285ea
trizin <25263018+trizin@users.noreply.github.com>
2023-05-17 13:03:30 +0300

    Implement tests to validate database table structures

---
+++ test/validate_db.test.js
@@ -0,0 +1,44 @@
+const { Parser } = require("node-sql-parser")
+const parser = new Parser()
+const {
+    allocationsTable,
+    nftVolsTable,
+    vebalsTable,
+    rewardsInfo,
+    passiveRewardsInfo,
+    nftinfoTable,
+    rewardsSummary,
+    ownersInfo
+} = require("../db/structure")
+
+describe("SQL Table Structures", () => {
+    test.each([
+        ["allocationsTable", allocationsTable],
+        ["nftVolsTable", nftVolsTable],
+        ["vebalsTable", vebalsTable],
+        ["rewardsInfo", rewardsInfo],
+        ["passiveRewardsInfo", passiveRewardsInfo],
+        ["nftinfoTable", nftinfoTable],
+        ["rewardsSummary", rewardsSummary],
+        ["ownersInfo", ownersInfo]
+    ])("validates structure of %s", (tableName, sqlQuery) => {
+        let ast
+        try {
+            ast = parser.astify(sqlQuery)
+        } catch (error) {
+            throw new Error(
+                `Error parsing SQL for ${tableName}: ${error.message}`
+            )
+        }
+        if (sqlQuery.endsWith(";")) {
+            // parser returns an array if the query ends with a ;
+            ast.forEach((ast) => {
+                expect(ast.type).toBe("create")
+                expect(ast.keyword).toBe("table")
+            })
+        } else {
+            expect(ast.type).toBe("create")
+            expect(ast.keyword).toBe("table")
+        }
+    })
+})
