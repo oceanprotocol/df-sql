add35d75287f75db1592a603b33bfcd06313ba3e
trizin <25263018+trizin@users.noreply.github.com>
2023-11-30 14:49:31 +0300

    Concurrency limit

--- src/cron/main.js
+++ src/cron/main.js
@@ -17,12 +17,29 @@ async function sync_historical() {
         return console.log("no historical data dir");
     }
 
-    let folders = fs.readdirSync(histDataDir);
+    const folders = fs.readdirSync(histDataDir);
 
-    for (const folder of folders) {
-        let roundNumber = parseInt(folder);
-        if (roundNumber) {
-            await Promise.all([sync(histDataDir + folder + "/", roundNumber)]);
+    const maxConcurrency = 20;
+
+    async function runWithConcurrency(tasks) {
+        const results = [];
+        while (tasks.length > 0) {
+            const batch = tasks.splice(0, maxConcurrency);
+            const batchResults = await Promise.all(batch.map(task => task()));
+            results.push(...batchResults);
         }
+        return results;
     }
+
+    const tasks = folders.map(folder => async () => {
+        const roundNumber = parseInt(folder);
+        if (roundNumber) {
+            await sync(histDataDir + folder + "/", roundNumber);
+            return `Sync completed for folder: ${folder}`;
+        }
+    });
+
+    const results = await runWithConcurrency(tasks);
+
+    results.forEach(result => console.log(result));
 }
