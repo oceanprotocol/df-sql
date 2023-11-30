c1a447e83c2a345b19b84831ebe75be3ea48ad67
trizin <25263018+trizin@users.noreply.github.com>
2023-11-30 16:52:29 +0300

    Do not wait for historical in live sync

--- src/cron/main.js
+++ src/cron/main.js
@@ -10,7 +10,7 @@ let syncing_historical = false;
 let syncing = false;
 
 croner.Cron("0 */1 * * * *", async () => {
-    if (!syncing && !syncing_historical) {
+    if (!syncing) {
         syncing = true;
         try {
             await sync(dataDir, 0)
